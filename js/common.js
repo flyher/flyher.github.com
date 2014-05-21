1645c003c7b1e5b9160dcee027eb42c4d6a47def
flyher <dong3580@gmail.com>
2014-05-21 11:29:33 +0800

    build

---
+++ js/common.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+	$.fn.fullpage({
+		slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+		menu: '#menu'
+	});
+});
